edd30ce19543112986224b000848d61a2175c2ad
B4sso <bicienzocorleone@hotmail.it>
2017-10-27 18:54:27 +0200

    Aggiornato esercizio

--- Bookshop/Bookshop.java
+++ Bookshop/Bookshop.java
@@ -1,21 +1,65 @@
+/*
+ * Bookshop consente di istanziare un oggetto di tipo Bookshop che contiene una serie di "Shelves".
+ * Ogni Shelf indirizza una serie di copie di un determinato oggetto (in quest'implementazione, un tipo Libro) con il relativo numero disponibile.
+ * Il Bookshop pu� essere importato da un file .txt che rispetti la seguente struttura:
+ * 	
+ * 	DATABASE.TXT
+ * 
+ * 	Nomelibro (Stringa)
+ *  Autore (Stringa)
+ *  Casa editrice (Stringa)
+ *  Edizione (int)
+ *  
+ *  Alternativamente lo si pu� generare da un ArrayList di Shelf \ da un solo Shelf, oppure si pu� creare un Bookshop vuoto, per poi
+ *  riempirlo. Tutti e tre i casi generano un database: db.txt | db_n.txt, dove n � il numero di database presenti in cartella lib (che viene generata
+ *  se assente).
+ *  
+ *	COSTRUTTORI:
+ *	|Bookshop(<Shelf>) -> Restituisce un Bookshop con <Shelf> elementi e database: db.txt | db_n.txt.
+ *	|Bookshop(File) -> Restiuisce un Bookshop di File.<Shelf> elementi e database: File
+ *	|Bookshop(Void) -> Restuisce un Bookshop di 0 elementi e database: db.txt | db_n.txt 
+ *
+ *	METODI:
+ *	|lookAuthor(Nome) -> Restituisce tutti gli oggetti con Autore == Nome
+ *	|look(Stringa) -> Restituisce tutti gli oggetti il cui Titolo contengano Stringa
+ *	|lookCopy(amount,flag) -> Restituisce tutti gli oggetti le cui sono <= amount o >= amount se !flag o flag
+ *	|addBook(Title,amount) -> Aggiunge amount copie allo Shelf di Titolo == Title
+ * 	
+ */
+
+
 import java.io.*;
 import java.util.*;
+
 public class Bookshop{
 	public Bookshop(ArrayList<Shelf> batch)
 	{
-		new File("lib").mkdir();
-		File temp;
-		if (!(temp = File(PATH+EXT).createNewFile()))
-		{
-			for (int i = 1;i<=10;i++)
-				if ((temp = File(PATH+Integer.toString(i)+EXT).createNewFile()))
-					break;
-			if (i > 10) return;
-		}
-		database = temp;
-		PrintStream printer = new PrintStream(database);
+		this.makeDB();
 		inventario = batch.clone();
+		this.writeDB();
 		batch.removeAll();
+	}
+	
+	public Bookshop(File sourceDB)
+	{
+		this.readDB(sourceDB);
+		database = sourceDB;
+	}
+	public Bookshop(){}
+	
+	private void readDB(File input)
+	{
+		Scanner stream = new Scanner(database);
+		while (stream.hasNextLine())
+			inventario.add(new Item(
+							stream.nextLine(),	//Titolo							
+							stream.nextLine(), //Autore
+							stream.nextLine()) //Casa editrice
+						  );	
+	}
+	private void writeDB()
+	{
+		PrintStream printer = new PrintStream(database);
 		for (int j = 0; j < inventario.length(); j++)
 		{
 			String[]details = inventario[j].getDetails();		
@@ -25,17 +69,70 @@ public class Bookshop{
 		}	
 	}
 	
-	public Bookshop(File sourceDB)
+	private void makeDB()
 	{
-		database = sourceDB;
-		Scanner stream = new Scanner(database);
-		while (stream.hasNextLine())
-			inventario.add(new Item(
-							stream.nextLine(),	//Titolo							
-							stream.nextLine(), //Autore
-							stream.nextLine()) //Editore
-							Integer.parseInt(stream.nextLine()); //Edizione
-						  );
+		new File("lib").mkdir();
+		File temp;
+		if (!(temp = File(PATH+EXT).createNewFile()))
+		{
+			for (int i = 1;i<=10;i++)
+				if ((temp = File(PATH+Integer.toString(i)+EXT).createNewFile()))
+					break;
+			if (i > 10) database = null;
+		}
+		database = temp;
+		return;
+	}
+	public ArrayList<Item> lookAuthor(String name)
+	{
+		ArrayList<Item> results;
+		for(Item e: inventario)
+		{
+			String[]tmp = e.getDetails();
+			if (tmp[1].equalsIgnoreCase(name))
+				results.add(e);
+		}
+		return results;
+	}
+	
+	public ArrayList<Item> look(String target)
+	{
+		ArrayList<Item> results;
+		for(Item e: inventario)
+		{
+			String[]tmp = e.getDetails();
+			if (tmp[0].toLowerCase().contains(target.toLowerCase()))
+				results.add(e);
+		}
+		return results;
+	}
+	
+	public ArrayList<Item> lookCopy(int amount, Boolean flag)
+	{
+		ArrayList<Item> results;
+		for(Item e: inventario)
+		{
+			String[]tmp = e.getDetails();
+			if (tmp[1].equalsIgnoreCase(name))
+				if (flag)
+					if(e.getCopies() == amount)
+						results.add(e);
+						else if (e.getCopies() <= amount)
+							results.add(e);
+		}
+		return results;
+	}
+
+	public void addBook(String title, int amount)
+	{
+		for(Item e: inventario)
+		{
+			String[]tmp = e.getDetails();
+			if (tmp[0].equalsIgnoreCase(name))
+				e.add(amount);
+				return;
+		}
+
 	}
 	
 	private ArrayList<Shelf> inventario;
